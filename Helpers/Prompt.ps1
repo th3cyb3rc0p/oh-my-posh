bdcf7e23e9a853b0224391f2c7cefa477f1ad660
Linard Hug <linard.hug@googlemail.com>
2020-05-22 19:04:36 +0200

    Add switch to not abbreviate home path for themes
    Helper function Get-FullPath has optional switch -noHomeAbbreviation
    which enables Themes to display paths in the prompt that are never
    abbreviated with the home symbol.

--- Helpers/Prompt.ps1
+++ Helpers/Prompt.ps1
@@ -99,13 +99,25 @@ function Get-FullPath {
     param(
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.PathInfo]
-        $dir
+        $dir,
+
+        [Parameter(Mandatory = $false)]
+        [switch]
+        $noHomeAbbreviation
     )
 
     if ($dir.path -eq "$($dir.Drive.Name):\") {
         return "$($dir.Drive.Name):"
     }
-    $path = $dir.path.Replace((Get-Home), $sl.PromptSymbols.HomeSymbol).Replace('\', $sl.PromptSymbols.PathSeparator)
+
+    if ($noHomeAbbreviation.IsPresent) {
+        $path = $dir.path
+    }
+    else {
+        $path = $dir.path.Replace((Get-Home), $sl.PromptSymbols.HomeSymbol)
+    }
+    $path = $path.Replace('\', $sl.PromptSymbols.PathSeparator)
+
     return $path
 }
 
